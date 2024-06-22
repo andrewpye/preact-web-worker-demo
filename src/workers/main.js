1a86c18cbe2c8269feec76cfbf7a6cf9ee896205
Andy Pye <apye@sweettoothhq.com>
2024-06-22 09:59:22 +0300

    Fix grammar

--- src/workers/main.js
+++ src/workers/main.js
@@ -2,6 +2,6 @@ import { render } from 'https://unpkg.com/preact@latest?module'
 import { html } from 'https://unpkg.com/htm/preact/index.module.js?module';
 import Counter from '../components/Counter.js';
 
-import '../utils/dom/setupDom.js';
+import '../utils/dom/setupUpDom.js';
 
 render(html`<${Counter} />`, document.body);
