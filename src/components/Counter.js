bb1db3c4bffa7047d93e07e4af2fb4c5ba445623
Andy Pye <apye@sweettoothhq.com>
2024-06-23 19:06:56 +0300

    Wire up start/stop button

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,41 @@
-import { useEffect, useState } from 'https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module';
+import { useCallback, useEffect, useRef, useState } from 'https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module';
 import { html } from 'https://unpkg.com/htm/preact/index.module.js?module';
 
 const Counter = () => {
+  const timerRef = useRef(null);
   const [count, setCount] = useState(0);
   const [isRunning, setIsRunning] = useState(true);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
+  const start = useCallback(() => {
+    timerRef.current = setInterval(() => {
       setCount((c) => c + 1);
     }, 1_000);
-
-    return () => {
-      clearInterval(timer);
-    };
   }, []);
+
+  const stop = useCallback(() => {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  }, []);
+
+  useEffect(() => {
+    if (isRunning) {
+      start();
+      return () => stop();
+    }
+
+    stop();
+  }, [isRunning]);
 
   return html`
     <div data-count=${count}>
       Count: ${count}
     </div>
 
-    <button onClick=${() => setIsRunning((r) => !r)}>
+    <!-- 
+      Probably something to do with htm, but the event name has to be
+      lower-case for Preact to run the event handler without throwing.
+    -->
+    <button onclick=${() => setIsRunning((r) => !r)}>
       ${isRunning ? 'Stop' : 'Start'}
     </button>
   `;
