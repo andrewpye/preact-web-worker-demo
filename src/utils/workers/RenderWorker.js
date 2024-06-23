892187d5eb452e1f1f3d21fc559a29335e3a7d1c
Andy Pye <apye@sweettoothhq.com>
2024-06-23 18:05:57 +0300

    Forward events from RenderWorker to thread

--- src/utils/workers/RenderWorker.js
+++ src/utils/workers/RenderWorker.js
@@ -1,10 +1,12 @@
 import DomMutationHandler from '../dom/DomMutationHandler.js';
+import EventForwarder from '../dom/EventForwarder.js';
 
 export default class RenderWorker extends Worker {
   constructor(scriptUrl, { container, ...options }) {
     super(scriptUrl, options);
 
     this._domMutationHandler = new DomMutationHandler({ container });
+    this._eventForwarder = new EventForwarder((e) => this._forwardEvent(e));
 
     this.onmessage = this._handleMessage;
   }
@@ -13,5 +15,9 @@ export default class RenderWorker extends Worker {
     if (data.type === 'domMutations') {
       this._domMutationHandler.processMutations(data.mutations);
     }
+  }
+
+  _forwardEvent(event) {
+    this.postMessage({ type: 'event', event });
   }
 }
