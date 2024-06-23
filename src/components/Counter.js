2a5e262afa84c3e5a955d9b9cfd7b108b46ba3c3
Andy Pye <apye@sweettoothhq.com>
2024-06-23 18:32:06 +0300

    Add start/stop button to counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,7 @@ import { html } from 'https://unpkg.com/htm/preact/index.module.js?module';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -14,7 +15,15 @@ const Counter = () => {
     };
   }, []);
 
-  return html`<div data-count=${count}>Count: ${count}</div>`;
+  return html`
+    <div data-count=${count}>
+      Count: ${count}
+    </div>
+
+    <button onClick=${() => setIsRunning((r) => !r)}>
+      ${isRunning ? 'Stop' : 'Start'}
+    </button>
+  `;
 };
 
 export default Counter;
