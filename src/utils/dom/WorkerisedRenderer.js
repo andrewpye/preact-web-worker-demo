5ab2c70db64f3942894e6d2e6573359f40e80ac3
Andy Pye <apye@sweettoothhq.com>
2024-06-23 17:46:11 +0300

    Move virtual DOM initialisation into wrapper class

--- src/utils/dom/WorkerisedRenderer.js
+++ src/utils/dom/WorkerisedRenderer.js
@@ -1,15 +1,34 @@
-import initialiseVirtualDom from './initialiseVirtualDom.js';
+import createVirtualDom from './createVirtualDom.js';
+import DomMutationCollector from './DomMutationCollector.js';
+import DomMutationMessagePoster from './DomMutationMessagePoster.js';
+import hookDomMutations from './hookDomMutations.js';
+import hookTextContentChanges from './hookTextContentChanges.js';
 
 export default class WorkerisedRenderer {
   constructor(render) {
     this._render = render;
 
+    this._domMutationMessagePoster = new DomMutationMessagePoster();
+
+    this._domMutationCollector = new DomMutationCollector((mutations) => {
+      this._domMutationMessagePoster.sendMutations(mutations);
+    });
+
+    this.collectMutation = this.collectMutation.bind(this);
+    
     this._initialise();
   }
-
+  
   _initialise() {
-    initialiseVirtualDom();
+    createVirtualDom();
+
+    hookDomMutations(this.collectMutation);
+    hookTextContentChanges(this.collectMutation);
 
     this._render();
   }
+
+  collectMutation(...args) {
+    this._domMutationCollector.collectMutation(...args);
+  };
 }
