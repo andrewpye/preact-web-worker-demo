d8e82ffabf8723843f0044961a5fb5915bb3e4a6
Andy Pye <apye@sweettoothhq.com>
2024-06-25 19:39:17 +0300

    Remove blank line

--- src/workers/main.js
+++ src/workers/main.js
@@ -2,7 +2,6 @@ import { render } from 'https://unpkg.com/preact@latest?module'
 import { html } from 'https://unpkg.com/htm/preact/index.module.js?module';
 import Counter from '../components/Counter.js';
 import WorkerSafeFrame from '../components/WorkerSafeFrame.js';
-
 import WorkerisedRenderer from '../utils/dom/WorkerisedRenderer.js';
 
 new WorkerisedRenderer(
