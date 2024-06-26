a6de353cb2f98465d95d9c45f8cc09631eba6d71
Andy Pye <apye@sweettoothhq.com>
2024-06-26 12:50:19 +0300

    Replace hacky frame document.write stubbing with more direct content updating

--- src/components/WorkerSafeFrame.js
+++ src/components/WorkerSafeFrame.js
@@ -3,41 +3,71 @@ import Frame from './Frame/Frame.js';
 export default class WorkerSafeFrame extends Frame {
   _hasWrittenInitialContent = false;
 
-  getMountTarget() {
-    // Fall back to the body since initial content that would be written
-    // via document.write() won't be written in the worker.
-    return super.getMountTarget() ?? this.getDoc().body;
-  }
-
   renderFrameContents() {
     if (this._hasWrittenInitialContent) {
       return super.renderFrameContents();
     }
 
-    // Run the document.write() call, but because we're sending it to the main thread
-    // it's async so we wait until that completes before we call the super.
+    // Populating the initial content here prevents the base class from attempting
+    // to call document.write (which isn't implemented in undom's Document class).
     this._writeInitialContent();
   }
 
   async _writeInitialContent() {
-    if (!this._isMounted) {
-      return;
-    }
-
     const doc = this.getDoc();
     if (!doc) {
       return;
     }
 
-    if (doc.body.children.length < 1) {
-      await doc.write(this.props.initialContent); // async polyfill because we have to post to the main thread.
+    await new Promise((resolve) => {
+      const onHtmlParsed = ({ data }) => {
+        if (data.type !== 'htmlParsed') {
+          return;
+        }
 
-      // Stub document.write calls to prevent the base class getting stuck in
-      // a loop where it calls renderFrameContents and we end up here again.
-      doc.write = function() {};
-    }
+        removeEventListener('message', onHtmlParsed);
+
+        // TODO: extract this to a reusable utility and maybe DRY up with element creation in DomMutationHandler.
+        const convertObjectToElement = (o, d) => {
+          let e;
+          if (o.nodeType === 3) {
+            e = d.createTextNode(o.nodeValue);
+          }
+          else if (o.nodeType === 1) {
+            e = d.createElement(o.nodeName);
+
+            o.attributes.forEach(({ name, value }) => {
+              e.setAttribute(name, value);
+            });
+
+            o.childNodes.forEach((child) => {
+              e.appendChild(convertObjectToElement(child, d));
+            });
+          }
+
+          return e;
+        };
+
+        // Rebuild the document's contents from the parsed HTML.
+        doc.documentElement = convertObjectToElement(JSON.parse(data.tree), doc);
+
+        const childNodes = doc.documentElement.childNodes;
+        doc.head = childNodes.find((n) => n.nodeName === 'HEAD');
+        doc.body = childNodes.find((n) => n.nodeName === 'BODY');
+
+        resolve();
+      };
+
+      addEventListener('message', onHtmlParsed);
+      postMessage({
+        type: 'parseHtml',
+        text: this.props.initialContent,
+      });
+    });
 
     this._hasWrittenInitialContent = true;
-    this.renderFrameContents();
+
+    // Trigger a re-render to flush the real frame contents to the virtual DOM.
+    this.forceUpdate();
   }
 };
